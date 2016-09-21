whitefog425dev/hyper

commit ec156acdf3f58344e71be3a479c8c3f234c1ee93

[WIP] Use XO instead of Standard (#723)
* Bump `eslint-plugin-react`

* Add `eslint-config-xo-react`

* Add XO

* Remove eslint-related dependencies, add XO config and use XO as the linter

* Code style: Standard => XO :sparkles:

* Use xo property to ignore files

* Fix remaining errors

lib/components/tab.js
@@ -2,78 +2,93 @@ import React from 'react';
 import Component from '../component';
 
 export default class Tab extends Component {
-  constructor () {
+  constructor() {
     super();
-    this.hover = this.hover.bind(this);
-    this.blur = this.blur.bind(this);
+
+    this.handleHover = this.handleHover.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
     this.handleClick = this.handleClick.bind(this);
+
     this.state = {
       hovered: false
     };
   }
 
-  hover () {
-    this.setState({ hovered: true });
+  shouldComponentUpdate() {
+    return true;
   }
 
-  blur () {
-    this.setState({ hovered: false });
+  handleHover() {
+    this.setState({
+      hovered: true
+    });
   }
 
-  handleClick (event) {
+  handleBlur() {
+    this.setState({
+      hovered: false
+    });
+  }
+
+  handleClick(event) {
     const isLeftClick = event.nativeEvent.which === 1;
     const isMiddleClick = event.nativeEvent.which === 2;
 
     if (isLeftClick) {
-      this.props.isActive ? null : this.props.onSelect();
+      if (this.props.isActive === null) {
+        this.props.onSelect();
+      }
     } else if (isMiddleClick) {
       this.props.onClose();
     }
   }
 
-  template (css) {
-    const { isActive, isFirst, isLast, borderColor, hasActivity } = this.props;
-    const { hovered } = this.state;
+  template(css) {
+    const {isActive, isFirst, isLast, borderColor, hasActivity} = this.props;
+    const {hovered} = this.state;
 
-    return <li
-      onMouseEnter={ this.hover }
-      onMouseLeave={ this.blur }
-      onClick={ this.props.onClick }
-      style={{ borderColor }}
-      className={ css(
+    return (<li
+      onMouseEnter={this.handleHover}
+      onMouseLeave={this.handleBlur}
+      onClick={this.props.onClick}
+      style={{borderColor}}
+      className={css(
         'tab',
         isFirst && 'first',
         isActive && 'active',
         isFirst && isActive && 'firstActive',
         hasActivity && 'hasActivity'
-      ) }>
-        { this.props.customChildrenBefore }
-        <span
-          className={ css(
-            'text',
-            isLast && 'textLast',
-            isActive && 'textActive'
-          ) }
-          onClick={ this.handleClick }>
-          <span className={ css('textInner') }>
-            { this.props.text }
-          </span>
+      )}
+      >
+      { this.props.customChildrenBefore }
+      <span
+        className={css(
+          'text',
+          isLast && 'textLast',
+          isActive && 'textActive'
+        )}
+        onClick={this.handleClick}
+        >
+        <span className={css('textInner')}>
+          { this.props.text }
         </span>
-        <i
-          className={ css(
-            'icon',
-            hovered && 'iconHovered'
-          ) }
-          onClick={ this.props.onClose }>
-          <svg className={ css('shape') }>
-            <use xlinkHref='./dist/assets/icons.svg#close'></use>
-          </svg>
-        </i>
-        { this.props.customChildren }
-    </li>;
+      </span>
+      <i
+        className={css(
+          'icon',
+          hovered && 'iconHovered'
+        )}
+        onClick={this.props.onClose}
+        >
+        <svg className={css('shape')}>
+          <use xlinkHref="./dist/assets/icons.svg#close"/>
+        </svg>
+      </i>
+      { this.props.customChildren }
+    </li>);
   }
 
-  styles () {
+  styles() {
     return {
       tab: {
         color: '#ccc',

.eslintignore
@@ -1,12 +1,0 @@
-# build output
-dist
-build
-app/dist
-
-# dependencies
-node_modules
-app/node_modules
-
-# other
-app/static
-assets

app/auto-updater.js
@@ -1,17 +1,18 @@
-const { autoUpdater } = require('electron');
-const { version } = require('./package');
-const notify = require('./notify'); // eslint-disable-line no-unused-vars
+const {autoUpdater} = require('electron');
 const ms = require('ms');
+
+const notify = require('./notify'); // eslint-disable-line no-unused-vars
+const {version} = require('./package');
 
 // accepted values: `osx`, `win32`
 // https://nuts.gitbook.com/update-windows.html
-const platform = 'darwin' === process.platform
-  ? 'osx'
-  : process.platform;
+const platform = process.platform === 'darwin' ?
+  'osx' :
+  process.platform;
 const FEED_URL = `https://hyperterm-updates.now.sh/update/${platform}`;
 let isInit = false;
 
-function init () {
+function init() {
   autoUpdater.on('error', (err, msg) => {
     console.error('Error fetching updates', msg + ' (' + err.stack + ')');
   });
@@ -30,12 +31,14 @@ function init () {
 }
 
 module.exports = function (win) {
-  if (!isInit) init();
+  if (!isInit) {
+    init();
+  }
 
-  const { rpc } = win;
+  const {rpc} = win;
 
   const onupdate = (ev, releaseNotes, releaseName) => {
-    rpc.emit('update available', { releaseNotes, releaseName });
+    rpc.emit('update available', {releaseNotes, releaseName});
   };
 
   autoUpdater.on('update-downloaded', onupdate);

app/config.js
@@ -1,9 +1,10 @@
-const { dialog } = require('electron');
-const { homedir } = require('os');
-const { resolve } = require('path');
-const { readFileSync, writeFileSync } = require('fs');
-const gaze = require('gaze');
+const {homedir} = require('os');
+const {readFileSync, writeFileSync} = require('fs');
+const {resolve} = require('path');
 const vm = require('vm');
+
+const {dialog} = require('electron');
+const gaze = require('gaze');
 const notify = require('./notify');
 
 const path = resolve(homedir(), '.hyperterm.js');
@@ -11,14 +12,16 @@ const watchers = [];
 
 let cfg = {};
 
-function watch () {
+function watch() {
   gaze(path, function (err) {
-    if (err) throw err;
+    if (err) {
+      throw err;
+    }
     this.on('changed', () => {
       try {
         if (exec(readFileSync(path, 'utf8'))) {
           notify('HyperTerm configuration reloaded!');
-          watchers.forEach((fn) => fn());
+          watchers.forEach(fn => fn());
         }
       } catch (err) {
         dialog.showMessageBox({
@@ -31,12 +34,14 @@ function watch () {
 }
 
 let _str; // last script
-function exec (str) {
-  if (str === _str) return false;
+function exec(str) {
+  if (str === _str) {
+    return false;
+  }
   _str = str;
   const script = new vm.Script(str);
   const module = {};
-  script.runInNewContext({ module });
+  script.runInNewContext({module});
   if (!module.exports) {
     throw new Error('Error reading configuration: `module.exports` not set');
   }

app/index.js
@@ -1,21 +1,28 @@
-const { app, BrowserWindow, shell, Menu } = require('electron');
-const createRPC = require('./rpc');
-const createMenu = require('./menu');
+// Native
+const {resolve} = require('path');
+
+// Packages
+const {parse: parseUrl} = require('url');
+const {gitDescribe} = require('git-describe');
+const {app, BrowserWindow, shell, Menu} = require('electron');
 const uuid = require('uuid');
-const { resolve } = require('path');
-const { parse: parseUrl } = require('url');
 const fileUriToPath = require('file-uri-to-path');
 const isDev = require('electron-is-dev');
+
+// Ours
 const AutoUpdater = require('./auto-updater');
 const toElectronBackgroundColor = require('./utils/to-electron-background-color');
+const createMenu = require('./menu');
+const createRPC = require('./rpc');
 const notify = require('./notify');
-const { gitDescribe } = require('git-describe');
 
 app.commandLine.appendSwitch('js-flags', '--harmony');
 
 // set up config
 const config = require('./config');
+
 config.init();
+
 const plugins = require('./plugins');
 const Session = require('./session');
 
@@ -29,7 +36,9 @@ app.getWindows = () => new Set([...windowSet]); // return a clone
 // function to retrive the last focused window in windowSet;
 // added to app object in order to expose it to plugins.
 app.getLastFocusedWindow = () => {
-  if (!windowSet.size) return null;
+  if (!windowSet.size) {
+    return null;
+  }
   return Array.from(windowSet).reduce((lastWindow, win) => {
     return win.focusTime > lastWindow.focusTime ? win : lastWindow;
   });
@@ -40,7 +49,9 @@ if (isDev) {
 
   // Overide default appVersion which is set from package.json
   gitDescribe({customArguments: ['--tags']}, (error, gitInfo) => {
-    if (!error) app.setVersion(gitInfo.raw);
+    if (!error) {
+      app.setVersion(gitInfo.raw);
+    }
   });
 } else {
   console.log('running in prod mode');
@@ -54,11 +65,11 @@ const url = 'file://' + resolve(
 console.log('electron will open', url);
 
 app.on('ready', () => {
-  function createWindow (fn) {
+  function createWindow(fn) {
     let cfg = plugins.getDecoratedConfig();
 
     const [width, height] = cfg.windowSize || [540, 380];
-    const { screen } = require('electron');
+    const {screen} = require('electron');
 
     let startX = 50;
     let startY = 50;
@@ -69,7 +80,7 @@ app.on('ready', () => {
     const focusedWindow = BrowserWindow.getFocusedWindow() || app.getLastFocusedWindow();
     if (focusedWindow) {
       const points = focusedWindow.getPosition();
-      const currentScreen = screen.getDisplayNearestPoint({ x: points[0], y: points[1] });
+      const currentScreen = screen.getDisplayNearestPoint({x: points[0], y: points[1]});
 
       const biggestX = ((points[0] + 100 + width) - currentScreen.bounds.x);
       const biggestY = ((points[1] + 100 + height) - currentScreen.bounds.y);
@@ -138,7 +149,9 @@ app.on('ready', () => {
 
       // If no callback is passed to createWindow,
       // a new session will be created by default.
-      if (!fn) fn = (win) => win.rpc.emit('session add req');
+      if (!fn) {
+        fn = win => win.rpc.emit('session add req');
+      }
 
       // app.windowCallback is the createWindow callback
       // that can be setted before the 'ready' app event
@@ -155,11 +168,11 @@ app.on('ready', () => {
       }
     });
 
-    rpc.on('new', ({ rows = 40, cols = 100, cwd = process.env.HOME }) => {
+    rpc.on('new', ({rows = 40, cols = 100, cwd = process.env.HOME}) => {
       const shell = cfg.shell;
       const shellArgs = cfg.shellArgs && Array.from(cfg.shellArgs);
 
-      initSession({ rows, cols, cwd, shell, shellArgs }, (uid, session) => {
+      initSession({rows, cols, cwd, shell, shellArgs}, (uid, session) => {
         sessions.set(uid, session);
         rpc.emit('session add', {
           uid,
@@ -167,17 +180,17 @@ app.on('ready', () => {
           pid: session.pty.pid
         });
 
-        session.on('data', (data) => {
-          rpc.emit('session data', { uid, data });
+        session.on('data', data => {
+          rpc.emit('session data', {uid, data});
         });
 
-        session.on('title', (title) => {
+        session.on('title', title => {
           win.setTitle(title);
-          rpc.emit('session title', { uid, title });
+          rpc.emit('session title', {uid, title});
         });
 
         session.on('exit', () => {
-          rpc.emit('session exit', { uid });
+          rpc.emit('session exit', {uid});
           sessions.delete(uid);
         });
       });
@@ -186,7 +199,7 @@ app.on('ready', () => {
     // TODO: this goes away when we are able to poll
     // for the title ourseleves, instead of relying
     // on Session and focus/blur to subscribe
-    rpc.on('focus', ({ uid }) => {
+    rpc.on('focus', ({uid}) => {
       const session = sessions.get(uid);
       if (typeof session !== 'undefined' && typeof session.lastTitle !== 'undefined') {
         win.setTitle(session.lastTitle);
@@ -197,7 +210,7 @@ app.on('ready', () => {
         console.log('session not found by', uid);
       }
     });
-    rpc.on('blur', ({ uid }) => {
+    rpc.on('blur', ({uid}) => {
       const session = sessions.get(uid);
 
       if (session) {
@@ -207,7 +220,7 @@ app.on('ready', () => {
       }
     });
 
-    rpc.on('exit', ({ uid }) => {
+    rpc.on('exit', ({uid}) => {
       const session = sessions.get(uid);
 
       if (session) {
@@ -225,17 +238,17 @@ app.on('ready', () => {
       win.maximize();
     });
 
-    rpc.on('resize', ({ cols, rows }) => {
-      sessions.forEach((session) => {
-        session.resize({ cols, rows });
+    rpc.on('resize', ({cols, rows}) => {
+      sessions.forEach(session => {
+        session.resize({cols, rows});
       });
     });
 
-    rpc.on('data', ({ uid, data }) => {
+    rpc.on('data', ({uid, data}) => {
       sessions.get(uid).write(data);
     });
 
-    rpc.on('open external', ({ url }) => {
+    rpc.on('open external', ({url}) => {
       shell.openExternal(url);
     });
 
@@ -263,11 +276,11 @@ app.on('ready', () => {
     // If file is dropped onto the terminal window, navigate event is prevented
     // and his path is added to active session.
     win.webContents.on('will-navigate', (event, url) => {
-      var protocol = typeof url === 'string' && parseUrl(url).protocol;
+      const protocol = typeof url === 'string' && parseUrl(url).protocol;
       if (protocol === 'file:') {
         event.preventDefault();
-        let path = fileUriToPath(url).replace(/ /g, '\\ ');
-        rpc.emit('session data send', { data: path });
+        const path = fileUriToPath(url).replace(/ /g, '\\ ');
+        rpc.emit('session data send', {data: path});
       }
     });
 
@@ -282,7 +295,7 @@ app.on('ready', () => {
     // load plugins
     load();
 
-    const pluginsUnsubscribe = plugins.subscribe((err) => {
+    const pluginsUnsubscribe = plugins.subscribe(err => {
       if (!err) {
         load();
         win.webContents.send('plugins change');
@@ -337,16 +350,18 @@ app.on('ready', () => {
     const tpl = plugins.decorateMenu(createMenu({
       createWindow,
       updatePlugins: () => {
-        plugins.updatePlugins({ force: true });
+        plugins.updatePlugins({force: true});
       }
     }));
 
     // If we're on Mac make a Dock Menu
     if (process.platform === 'darwin') {
-      const { app, Menu } = require('electron');
-      const dockMenu = Menu.buildFromTemplate([
-        {label: 'New Window', click () { createWindow(); }}
-      ]);
+      const dockMenu = Menu.buildFromTemplate([{
+        label: 'New Window',
+        click() {
+          createWindow();
+        }
+      }]);
       app.dock.setMenu(dockMenu);
     }
 
@@ -362,16 +377,16 @@ app.on('ready', () => {
   plugins.subscribe(load);
 });
 
-function initSession (opts, fn) {
+function initSession(opts, fn) {
   fn(uuid.v4(), new Session(opts));
 }
 
 app.on('open-file', (event, path) => {
   const lastWindow = app.getLastFocusedWindow();
-  const callback = win => win.rpc.emit('open file', { path });
+  const callback = win => win.rpc.emit('open file', {path});
   if (lastWindow) {
     callback(lastWindow);
-  } else if (!lastWindow && app.hasOwnProperty('createWindow')) {
+  } else if (!lastWindow && {}.hasOwnProperty.call(app, 'createWindow')) {
     app.createWindow(callback);
   } else {
     // if createWindow not exists yet ('ready' event was not fired),

app/menu.js
@@ -1,12 +1,13 @@
 const os = require('os');
 const path = require('path');
-const { app, shell, dialog } = require('electron');
+const {app, shell, dialog} = require('electron');
+
 const appName = app.getName();
 
 // based on and inspired by
 // https://github.com/sindresorhus/anatine/blob/master/menu.js
 
-module.exports = function createMenu ({ createWindow, updatePlugins }) {
+module.exports = function createMenu({createWindow, updatePlugins}) {
   return [
     {
       label: 'Application',
@@ -20,7 +21,7 @@ module.exports = function createMenu ({ createWindow, updatePlugins }) {
         {
           label: 'Preferences...',
           accelerator: 'Cmd+,',
-          click (item, focusedWindow) {
+          click(item, focusedWindow) {
             if (focusedWindow) {
               focusedWindow.rpc.emit('preferences');
             } else {
@@ -61,14 +62,14 @@ module.exports = function createMenu ({ createWindow, updatePlugins }) {
         {
           label: 'New Window',
           accelerator: 'CmdOrCtrl+N',
-          click (item, focusedWindow) {
+          click() {
             createWindow();
           }
         },
         {
           label: 'New Tab',
           accelerator: 'CmdOrCtrl+T',
-          click (item, focusedWindow) {
+          click(item, focusedWindow) {
             if (focusedWindow) {
               focusedWindow.rpc.emit('session add req');
             } else {
@@ -82,7 +83,7 @@ module.exports = function createMenu ({ createWindow, updatePlugins }) {
         {
           label: 'Close',
           accelerator: 'CmdOrCtrl+W',
-          click (item, focusedWindow) {
+          click(item, focusedWindow) {
             if (focusedWindow) {
               focusedWindow.rpc.emit('session close req');
             }
@@ -125,7 +126,7 @@ module.exports = function createMenu ({ createWindow, updatePlugins }) {
         {
           label: 'Clear',
           accelerator: 'CmdOrCtrl+K',
-          click (item, focusedWindow) {
+          click(item, focusedWindow) {
             if (focusedWindow) {
               focusedWindow.rpc.emit('session clear req');
             }
@@ -139,7 +140,7 @@ module.exports = function createMenu ({ createWindow, updatePlugins }) {
         {
           label: 'Reload',
           accelerator: 'CmdOrCtrl+R',
-          click (item, focusedWindow) {
+          click(item, focusedWindow) {
             if (focusedWindow) {
               focusedWindow.rpc.emit('reload');
             }
@@ -148,7 +149,7 @@ module.exports = function createMenu ({ createWindow, updatePlugins }) {
         {
           label: 'Full Reload',
           accelerator: 'CmdOrCtrl+Shift+R',
-          click (item, focusedWindow) {
+          click(item, focusedWindow) {
             if (focusedWindow) {
               focusedWindow.reload();
             }
@@ -157,7 +158,7 @@ module.exports = function createMenu ({ createWindow, updatePlugins }) {
         {
           label: 'Toggle Developer Tools',
           accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
-          click (item, focusedWindow) {
+          click(item, focusedWindow) {
             if (focusedWindow) {
               focusedWindow.webContents.toggleDevTools();
             }
@@ -169,7 +170,7 @@ module.exports = function createMenu ({ createWindow, updatePlugins }) {
         {
           label: 'Reset Zoom Level',
           accelerator: 'CmdOrCtrl+0',
-          click (item, focusedWindow) {
+          click(item, focusedWindow) {
             if (focusedWindow) {
               focusedWindow.rpc.emit('reset fontSize req');
             }
@@ -178,7 +179,7 @@ module.exports = function createMenu ({ createWindow, updatePlugins }) {
         {
           label: 'Zoom In',
           accelerator: 'CmdOrCtrl+plus',
-          click (item, focusedWindow) {
+          click(item, focusedWindow) {
             if (focusedWindow) {
               focusedWindow.rpc.emit('increase fontSize req');
             }
@@ -187,7 +188,7 @@ module.exports = function createMenu ({ createWindow, updatePlugins }) {
         {
           label: 'Zoom Out',
           accelerator: 'CmdOrCtrl+-',
-          click (item, focusedWindow) {
+          click(item, focusedWindow) {
             if (focusedWindow) {
               focusedWindow.rpc.emit('decrease fontSize req');
             }
@@ -201,7 +202,7 @@ module.exports = function createMenu ({ createWindow, updatePlugins }) {
         {
           label: 'Update All Now',
           accelerator: 'CmdOrCtrl+Shift+U',
-          click () {
+          click() {
             updatePlugins();
           }
         }
@@ -222,7 +223,7 @@ module.exports = function createMenu ({ createWindow, updatePlugins }) {
         {
           label: 'Show Previous Tab',
           accelerator: 'CmdOrCtrl+Option+Left',
-          click (item, focusedWindow) {
+          click(item, focusedWindow) {
             if (focusedWindow) {
               focusedWindow.rpc.emit('move left req');
             }
@@ -231,7 +232,7 @@ module.exports = function createMenu ({ createWindow, updatePlugins }) {
         {
           label: 'Show Next Tab',
           accelerator: 'CmdOrCtrl+Option+Right',
-          click (item, focusedWindow) {
+          click(item, focusedWindow) {
             if (focusedWindow) {
               focusedWindow.rpc.emit('move right req');
             }
@@ -253,13 +254,13 @@ module.exports = function createMenu ({ createWindow, updatePlugins }) {
       submenu: [
         {
           label: `${appName} Website`,
-          click () {
+          click() {
             shell.openExternal('https://hyperterm.now.sh');
           }
         },
         {
           label: 'Report an Issue...',
-          click () {
+          click() {
             const body = `
 <!-- Please succinctly describe your issue and steps to reproduce it. -->
 
@@ -273,12 +274,13 @@ ${process.platform} ${process.arch} ${os.release()}`;
           }
         },
         ...(
-          'darwin' !== process.platform
-            ? [
-                { type: 'separator' },
+          process.platform === 'darwin' ?
+            [] :
+            [
+                {type: 'separator'},
               {
                 role: 'about',
-                click () {
+                click() {
                   dialog.showMessageBox({
                     title: `About ${appName}`,
                     message: `${appName} ${app.getVersion()}`,
@@ -289,7 +291,6 @@ ${process.platform} ${process.arch} ${os.release()}`;
                 }
               }
             ]
-            : []
         )
       ]
     }

app/notify.js
@@ -1,6 +1,7 @@
-const { app, BrowserWindow } = require('electron');
+const {resolve} = require('path');
+
+const {app, BrowserWindow} = require('electron');
 const isDev = require('electron-is-dev');
-const { resolve } = require('path');
 
 let win;
 
@@ -29,10 +30,10 @@ app.on('ready', () => {
   });
 });
 
-function notify (title, body) {
+function notify(title, body) {
   console.log(`[Notification] ${title}: ${body}`);
   if (win) {
-    win.webContents.send('notification', { title, body });
+    win.webContents.send('notification', {title, body});
   } else {
     buffer.push([title, body]);
   }

app/plugins.js
@@ -1,14 +1,16 @@
-const { app, dialog } = require('electron');
-const { homedir } = require('os');
-const { resolve, basename } = require('path');
-const { writeFileSync } = require('fs');
-const config = require('./config');
-const { sync: mkdirpSync } = require('mkdirp');
-const { exec } = require('child_process');
+const {exec} = require('child_process');
+const {homedir} = require('os');
+const {resolve, basename} = require('path');
+const {writeFileSync} = require('fs');
+
+const {app, dialog} = require('electron');
+const {sync: mkdirpSync} = require('mkdirp');
 const Config = require('electron-config');
 const ms = require('ms');
-const notify = require('./notify');
 const shellEnv = require('shell-env');
+
+const config = require('./config');
+const notify = require('./notify');
 
 // local storage
 const cache = new Config();
@@ -34,7 +36,7 @@ let paths = getPaths(plugins);
 let id = getId(plugins);
 let modules = requirePlugins();
 
-function getId (plugins_) {
+function getId(plugins_) {
   return JSON.stringify(plugins_);
 }
 
@@ -56,12 +58,14 @@ config.subscribe(() => {
 
 let updating = false;
 
-function updatePlugins ({ force = false } = {}) {
-  if (updating) return notify('Plugin update in progress');
+function updatePlugins({force = false} = {}) {
+  if (updating) {
+    return notify('Plugin update in progress');
+  }
   updating = true;
   syncPackageJSON();
   const id_ = id;
-  install((err) => {
+  install(err => {
     updating = false;
 
     if (err) {
@@ -109,15 +113,15 @@ function updatePlugins ({ force = false } = {}) {
             'No changes!'
           );
         }
-        watchers.forEach((fn) => fn(err, { force }));
+        watchers.forEach(fn => fn(err, {force}));
       }
     }
   });
 }
 
-function getPluginVersions () {
+function getPluginVersions() {
   const paths_ = paths.plugins.concat(paths.localPlugins);
-  return paths_.map((path) => {
+  return paths_.map(path => {
     let version = null;
     try {
       version = require(resolve(path, 'package.json')).version;
@@ -129,10 +133,12 @@ function getPluginVersions () {
   });
 }
 
-function clearCache (mod) {
+function clearCache() {
   // trigger unload hooks
-  modules.forEach((mod) => {
-    if (mod.onUnload) mod.onUnload(app);
+  modules.forEach(mod => {
+    if (mod.onUnload) {
+      mod.onUnload(app);
+    }
   });
 
   // clear require cache
@@ -159,7 +165,7 @@ if (cache.get('hyperterm.plugins') !== id || process.env.HYPERTERM_FORCE_UPDATE)
 // otherwise update plugins every 5 hours
 setInterval(updatePlugins, ms('5h'));
 
-function syncPackageJSON () {
+function syncPackageJSON() {
   const dependencies = toDependencies(plugins);
   const pkg = {
     name: 'hyperterm-plugins',
@@ -180,16 +186,16 @@ function syncPackageJSON () {
   }
 }
 
-function alert (message) {
+function alert(message) {
   dialog.showMessageBox({
     message,
     buttons: ['Ok']
   });
 }
 
-function toDependencies (plugins) {
+function toDependencies(plugins) {
   const obj = {};
-  plugins.plugins.forEach((plugin) => {
+  plugins.plugins.forEach(plugin => {
     const regex = /.(@|#)/;
     const match = regex.exec(plugin);
 
@@ -207,22 +213,28 @@ function toDependencies (plugins) {
   return obj;
 }
 
-function install (fn) {
-  const { shell: cfgShell, npmRegistry } = exports.getDecoratedConfig();
+function install(fn) {
+  const {shell: cfgShell, npmRegistry} = exports.getDecoratedConfig();
 
   const shell = cfgShell && cfgShell !== '' ? cfgShell : undefined;
 
-  shellEnv(shell).then((env) => {
-    if (npmRegistry) env.NPM_CONFIG_REGISTRY = npmRegistry;
+  shellEnv(shell).then(env => {
+    if (npmRegistry) {
+      env.NPM_CONFIG_REGISTRY = npmRegistry;
+    }
+    /* eslint-disable camelcase  */
     env.npm_config_runtime = 'electron';
     env.npm_config_target = '1.3.0';
     env.npm_config_disturl = 'https://atom.io/download/atom-shell';
+    /* eslint-enable camelcase  */
     exec('npm prune; npm install --production', {
       cwd: path,
       env,
       shell
-    }, (err, stdout, stderr) => {
-      if (err) return fn(err);
+    }, err => {
+      if (err) {
+        return fn(err);
+      }
       fn(null);
     });
   }).catch(fn);
@@ -235,12 +247,12 @@ exports.subscribe = function (fn) {
   };
 };
 
-function getPaths () {
+function getPaths() {
   return {
-    plugins: plugins.plugins.map((name) => {
+    plugins: plugins.plugins.map(name => {
       return resolve(path, 'node_modules', name.split('#')[0]);
     }),
-    localPlugins: plugins.localPlugins.map((name) => {
+    localPlugins: plugins.localPlugins.map(name => {
       return resolve(localPath, name);
     })
   };
@@ -251,13 +263,13 @@ exports.getPaths = getPaths;
 
 // get paths from renderer
 exports.getBasePaths = function () {
-  return { path, localPath };
+  return {path, localPath};
 };
 
-function requirePlugins () {
-  const { plugins, localPlugins } = paths;
+function requirePlugins() {
+  const {plugins, localPlugins} = paths;
 
-  const load = (path) => {
+  const load = path => {
     let mod;
     try {
       mod = require(path);
@@ -279,11 +291,11 @@ function requirePlugins () {
 
   return plugins.map(load)
     .concat(localPlugins.map(load))
-    .filter(v => !!v);
+    .filter(v => Boolean(v));
 }
 
 exports.onApp = function (app) {
-  modules.forEach((plugin) => {
+  modules.forEach(plugin => {
     if (plugin.onApp) {
       plugin.onApp(app);
     }
@@ -291,7 +303,7 @@ exports.onApp = function (app) {
 };
 
 exports.onWindow = function (win) {
-  modules.forEach((plugin) => {
+  modules.forEach(plugin => {
     if (plugin.onWindow) {
       plugin.onWindow(win);
     }
@@ -300,12 +312,12 @@ exports.onWindow = function (win) {
 
 // decorates the base object by calling plugin[key]
 // for all the available plugins
-function decorateObject (base, key) {
+function decorateObject(base, key) {
   let decorated = base;
-  modules.forEach((plugin) => {
+  modules.forEach(plugin => {
     if (plugin[key]) {
       const res = plugin[key](decorated);
-      if (res && 'object' === typeof res) {
+      if (res && typeof res === 'object') {
         decorated = res;
       } else {
         notify('Plugin error!', `"${plugin._name}": invalid return type for \`${key}\``);

commit 53908d6159c05511be91a554dcd87751706332f4

Fix tab navigation using mouse clicks (#732)
Fixes a bug introduced by #723

lib/components/tab.js
@@ -34,10 +34,8 @@ export default class Tab extends Component {
     const isLeftClick = event.nativeEvent.which === 1;
     const isMiddleClick = event.nativeEvent.which === 2;
 
-    if (isLeftClick) {
-      if (this.props.isActive === null) {
-        this.props.onSelect();
-      }
+    if (isLeftClick && !this.props.isActive) {
+      this.props.onSelect();
     } else if (isMiddleClick) {
       this.props.onClose();
     }